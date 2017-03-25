a8bd142da313723675c6da37e94b816d2da87898
Braulio <braulio.padilla@arrayent.com>
2017-03-24 19:09:04 -0600

    adding monitors

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,43 @@
-output "CPU-monitor-ID" {
-  value = "${datadog_monitor.CPU.id}"
+output "CPU-iowait-monitor-ID" {
+  value = "${datadog_monitor.CPU_iowait.id}"
+}
+
+output "CPU-system-monitor-ID" {
+  value = "${datadog_monitor.CPU_system.id}"
+}
+
+output "CPU-user-monitor-ID" {
+  value = "${datadog_monitor.CPU_user.id}"
+}
+
+output "CPU-stolen-monitor-ID" {
+  value = "${datadog_monitor.CPU_stolen.id}"
 }
 
 output "MEMORY-monitor-ID" {
   value = "${datadog_monitor.MEMORY.id}"
+}
+
+output "DISK-monitor-ID" {
+  value = "${datadog_monitor.DISK.id}"
+}
+
+output "LOAD_AVERAGE_1MINUTES-ID" {
+  value = "${datadog_monitor.LOAD_AVERAGE_1MINUTES.id}"
+}
+
+output "LOAD_AVERAGE_15MINUTES-ID" {
+  value = "${datadog_monitor.LOAD_AVERAGE_15MINUTES.id}"
+}
+
+output "LOAD_AVERAGE_5MINUTES-ID" {
+  value = "${datadog_monitor.LOAD_AVERAGE_5MINUTES.id}"
+}
+
+output "SWAP_MEMORY-ID" {
+  value = "${datadog_monitor.SWAP_MEMORY.id}"
+}
+
+output "NTP_OFFSET-ID" {
+  value = "${datadog_monitor.NTP_OFFSET.id}"
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,3 @@
-datadog_api_key = "59fe3f4f555ce025a2793058eae90de7"
-datadog_app_key = "fa73c0f3e0966301f02dab54c232f3fb9ee96d00"
+datadog_api_key = "d0c5d5cd22b4ef22bc0b92f4a63f0bc5"
+datadog_app_key = "75358852ed7bca84b7b3eb0de2f18d0233b81709"
 enviroment_tag = "prod"
